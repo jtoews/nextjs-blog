4b29ed5582ef83f5aecb320dce6de3c1ed2c33f8
jtoews <john.toews@momentum.com>
2020-10-07 16:27:28 -0400

    adds component and global styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,19 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
 
-  import Link from 'next/link'
-
-  export default function FirstPost() {
+export default function FirstPost() {
     return (
-      <>
-        <h1>First Post</h1>
-        <h2>
-          <Link href="/">
-            <a>Back to home</a>
-          </Link>
-        </h2>
-      </>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+        </Layout>
     )
-  }
-    
+}
